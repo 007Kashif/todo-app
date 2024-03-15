6ded3b724c2d887b96bfc685ef8936eac3d2e5b6
kashif <73710039+007Kashif@users.noreply.github.com>
2024-03-15 10:44:44 +0500

    Update InviteContacts.js

--- src/screens/invitation/InviteContacts.js
+++ src/screens/invitation/InviteContacts.js
@@ -1,11 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, Image, FlatList, TouchableOpacity } from 'react-native';
-import Contacts from 'react-native-contacts';
+import { View, Text, Image, Linking, FlatList, TouchableOpacity } from 'react-native';
 
-import {
-    widthPercentageToDP as wp,
-    heightPercentageToDP as hp,
-} from 'react-native-responsive-screen';
+import Contacts from 'react-native-contacts';
+import dynamicLinks from '@react-native-firebase/dynamic-links';
 
 import { styles } from './styles';
 import { images } from '../../assets/images/images';
@@ -58,13 +55,58 @@ export const InviteContacts = () => {
                     <Text>{item.phoneNumbers?.[0]?.number}</Text>
                 </View>
             </View>
-            <TouchableOpacity onPress={() => alert(item.phoneNumbers?.[0]?.number)}>
+            <TouchableOpacity onPress={() => openMessageApp(item.phoneNumbers?.[0]?.number)}>
                 <Text
                     style={{ color: "#1E90FF", fontFamily: Fonts.SoraMedium }}>
                     Invite</Text>
             </TouchableOpacity>
         </View>
     );
+
+    const generateLink = async () => {
+        try {
+            const link = await dynamicLinks().buildShortLink({
+                link: `https://voicelifeapp123.page.link/dmCn?refCode=QAZ123`,
+                domainUriPrefix: 'https://voicelifeapp123.page.link',
+                android: {
+                    packageName: 'com.voicelifeapp',
+                },
+            }, dynamicLinks.ShortLinkType.DEFAULT)
+            console.log('link:', link)
+            return link
+        } catch (error) {
+            console.log('Generating Link Error:', error)
+        }
+    }
+
+    const openMessageApp = async (number) => {
+        const getLink = await generateLink()
+
+        const phoneNumber = number;
+        const message = getLink;
+        const encodedMessage = encodeURIComponent(message);
+
+        let messageAppLink;
+
+        // Construct the message app link based on the platform
+        if (Platform.OS === 'android') {
+            messageAppLink = `sms:${phoneNumber}?body=${encodedMessage}`;
+        } else if (Platform.OS === 'ios') {
+            messageAppLink = `sms:${phoneNumber}&body=${encodedMessage}`;
+        } else {
+            console.error('Unsupported platform.');
+            return;
+        }
+
+        // Open the messaging app
+        Linking.canOpenURL(messageAppLink).then(supported => {
+            if (supported) {
+                return Linking.openURL(messageAppLink);
+            } else {
+                console.error("Don't know how to open messaging app on this device.");
+            }
+        }).catch(err => console.error('An error occurred', err));
+    };
 
     return (
         <View style={styles.container}>
